08abe89c1b90cbb16cbd3ec99056ddcbb09a2668
Harald Lapp <harald.lapp@gmail.com>
2013-02-02 01:30:19 +0100

    tabs to spaces

--- demo/label.php
+++ demo/label.php
@@ -10,10 +10,10 @@ use \org\octris\ncurses as ncurses;
 $app = ncurses\app::getInstance();
 
 $app->addChild(
-	new ncurses\component\label(1, 1, 'Label #1')
+    new ncurses\component\label(1, 1, 'Label #1')
 );
 $app->addChild(
-	new ncurses\component\label(1, 2, 'Label #2')
+    new ncurses\component\label(1, 2, 'Label #2')
 );
 
 
--- demo/menu.php
+++ demo/menu.php
@@ -10,10 +10,10 @@ use \org\octris\ncurses as ncurses;
 $app = ncurses\app::getInstance();
 
 $menu = $app->addChild(
-	new ncurses\component\menu(array(
-		array('label' => 'Network'),
-		array('label' => 'Administration')
-	))
+    new ncurses\component\menu(array(
+        array('label' => 'Network'),
+        array('label' => 'Administration')
+    ))
 );
 
 $app->build();
