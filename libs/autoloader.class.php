6576bd98e68aff72ba9d43958b26b85015809e6f
Harald Lapp <harald.lapp@gmail.com>
2013-02-02 15:43:09 +0100

    initial commit autoloader

---
+++ libs/autoloader.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.ncurses' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\ncurses {
+    /**
+     * Class Autoloader.
+     *
+     * @octdoc      c:app/autoloader
+     * @copyright   copyright (c) 2010-2013 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class autoloader
+    /**/
+    {
+        /**
+         * Class Autoloader.
+         *
+         * @octdoc  m:app/autoload
+         * @param   string      $classpath      Path of class to load.
+         */
+        public static function autoload($classpath)
+        /**/
+        {
+            $pkg = preg_replace('|\\\\|', '/', preg_replace('|\\\\|', '.', ltrim($classpath, '\\\\'), 2)) . '.class.php';
+
+            try {
+                include_once($pkg);
+            } catch(\Exception $e) {
+            }
+        }
+    }
+
+    spl_autoload_register(array('\org\octris\ncurses\autoloader', 'autoload'));
+}
