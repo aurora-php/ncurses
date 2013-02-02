0ce663023e59a9eafab6cc35b89f9f8dba0137e9
Harald Lapp <harald.lapp@gmail.com>
2013-02-02 03:03:55 +0100

    initial commit checkbox component and demo

---
+++ demo/checkbox.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+
+define('NCURSES_LOG', '/tmp/test.log');
+
+require_once('org.octris.core/app/autoloader.class.php');
+
+use \org\octris\ncurses as ncurses;
+
+$app = ncurses\app::getInstance();
+$win = $app->addChild(new ncurses\component\window(
+	14, 4, 5, 5
+));
+
+$menu = $win->addChild(
+    new ncurses\component\checkbox(array(
+        array('label' => 'Apple', 'selected' => true),
+        array('label' => 'Orange', 'selected' => false)
+    ), 1, 1)
+);
+
+$app->build();
+$app->refresh();
+
+$menu->run();
---
+++ libs/component/checkbox.class.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.ncurses' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\ncurses\component {
+    /**
+     * checkbox component.
+     *
+     * @octdoc      c:component/checkbox
+     * @copyright   copyright (c) 2013 by Harald Lapp
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class checkbox extends \org\octris\ncurses\component\listbox
+    /**/
+    {
+        /**
+         * Constructor.
+         *
+         * @octdoc  m:checkbox/__construct
+         * @param   int             $items          Items of checkbox.
+         * @param   int             $x              Optional x position of checkbox.
+         * @param   int             $y              Optional y position of checkbox.
+         */
+        public function __construct(array $items, $x = 0, $y = 0)
+        /**/
+        {
+            $this->height = count($items);
+            $this->width  = array_reduce($items, function(&$width, $item) {
+                $width = max($width, strlen($item['label']) + 6);
+
+                return $width;
+            }, 0);
+
+            array_walk($items, function(&$item, $no) {
+                $item['label'] = str_pad(
+                    ' [' . ($item['selected'] ? 'X' : ' ') . '] ' . $item['label'], 
+                    $this->width, 
+                    ' ', 
+                    STR_PAD_RIGHT
+                );
+
+                if (isset($item['action'])) {
+                    $action = $item['action'];
+
+                    $item['action'] = function() use ($action, $no) {
+                        $this->toggle($no + 1);
+
+                        $action();
+                    };
+                } else {
+                    $item['action'] = function() use ($no) {
+                        $this->toggle($no + 1);
+                    };
+                }
+            });
+
+            $this->items = $items;
+            $this->cnt   = count($items);
+
+            $this->x = $x;
+            $this->y = $y;
+        }
+
+        /**
+         * Toggle checkbox item.
+         *
+         * @octdoc  m:checkbox/toggle
+         * @param   int                     $no             Number of item to toggle.
+         */
+        public function toggle($no)
+        /**/
+        {
+            if ($no < 1 || $no > $this->cnt) return;
+
+            $res      = $this->parent->getResource();
+            $selected = ($this->items[$no - 1]['selected'] = !$this->items[$no - 1]['selected']);
+
+            if ($no == $this->selected) {
+                ncurses_wattron($res, NCURSES_A_REVERSE);
+                ncurses_mvwaddstr(
+                    $res,
+                    $this->y + ($no - 1), 
+                    $this->x + 2, 
+                    ($selected ? 'X' : ' ')
+                );
+                ncurses_wattroff($res, NCURSES_A_REVERSE);
+            } else {
+                ncurses_mvwaddstr(
+                    $res,
+                    $this->y + ($no - 1), 
+                    $this->x + 2, 
+                    ($selected ? 'X' : ' ')
+                );
+            }
+
+            $this->parent->refresh();
+        }
+    }
+}
