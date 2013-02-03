568fbcdea45f83e82ac43121d67b16e5a99745b8
Harald Lapp <harald.lapp@gmail.com>
2013-02-03 12:29:55 +0100

    implemented event handlers for components

--- libs/component.class.php
+++ libs/component.class.php
@@ -48,6 +48,24 @@ namespace org\octris\ncurses {
         /**/
 
         /**
+         * Stores registered event handlers.
+         *
+         * @octdoc  p:component/$events
+         * @var     array
+         */
+        protected $events = array();
+        /**/
+
+        /**
+         * Stores event handler IDs.
+         *
+         * @octdoc  p:component/$events_cnt
+         * @var     array
+         */
+        protected $events_cnt = array();
+        /**/
+
+        /**
          * Set parent container for component.
          *
          * @octdoc  m:component/setParent
@@ -68,6 +86,84 @@ namespace org\octris\ncurses {
         /**/
         {
             return $this->resource;
+        }
+
+        /**
+         * Add event handler.
+         *
+         * @octdoc  m:component/addEvent
+         * @param   string          $name               Name of event to add handler for.
+         * @param   callable        $cb                 Callback to trigger for event.
+         * @param   bool            $propagate          Whether to propagate event to other handlers.
+         * @return  string                              ID the event handler is registered as.
+         */
+        public function addEvent($name, callable $cb, $propagate = true)
+        /**/
+        {
+            $name = strtolower($name);
+
+            if (!isset($this->events_cnt[$name])) {
+                $no = $this->events_cnt[$name] = 1;
+            } else {
+                $no = ++$this->events_cnt[$name];
+            }
+
+            $id = $name . '-' . $no;
+
+            if (!isset($this->events[$name])) {
+                $this->events[$name] = array();
+            }
+
+            array_unshift($this->events[$name], array(
+                'id'        => $id,
+                'callback'  => $cb,
+                'propagate' => $propagate
+            ));
+
+            return $id;
+        }
+
+        /**
+         * Remove event handler.
+         *
+         * @octdoc  m:component/removeEvent
+         * @param   string              $id                 ID of event handler to remove.
+         */
+        public function removeEvent($id)
+        /**/
+        {
+            $id   = strtolower($id);
+            $name = explode('-', $id)[0];
+
+            if (isset($this->events[$name])) {
+                $this->events[$name] = array_filter(
+                    $this->events[$name], 
+                    function($item) use ($id) {
+                        return ($item['id'] != $id);
+                    }
+                );
+            }
+        }
+
+        /**
+         * Propagate event.
+         *
+         * @octdoc  m:component/propagateEvent
+         * @param   string              $name               Name of event to propagate.
+         * @param   array               $args               Optional arguments for event handler.
+         */
+        protected function propagateEvent($name, array $args = array())
+        /**/
+        {
+            $name = strtolower($name);
+
+            if (!isset($this->events[$name])) return;
+
+            foreach ($this->events[$name] as $handler) {
+                call_user_func_array($handler['callback'], $args);
+
+                if (!$handler['propagate']) break;
+            }
         }
 
         /**
@@ -103,7 +199,20 @@ namespace org\octris\ncurses {
         }
 
         /**
-         * Set focus for component.
+         * Event is triggered if component is focused and a key is pressed.
+         * Note however, that TAB keys will be handled by the container class.
+         *
+         * @octdoc  m:component/onKeypress
+         * @param   int                 $key            Code of the key that was pressed.
+         */
+        public function onKeypress($key_code)
+        /**/
+        {
+            $this->propagateEvent('keypress', array($key_code));
+        }
+
+        /**
+         * Move focus to the component.
          *
          * @octdoc  m:component/focus
          */
