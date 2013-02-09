eda20e2d1b333b8c3844f2e290f65d0fb5d17fa0
Harald Lapp <harald.lapp@gmail.com>
2013-02-09 16:11:53 +0100

    fixed example

--- demo/examples/dialog1.php
+++ demo/examples/dialog1.php
@@ -13,26 +13,26 @@ class win extends ncurses\container\window {
 	protected $checkbox;
 
 	protected function setup() {
-		$this->addChild(new ncurses\component\label(2, 1, 'Hostname'));
-		$this->addChild(new ncurses\component\textline(15, 1, 20));
-		$this->addChild(new ncurses\component\label(2, 2, 'Domain Name'));
-		$this->addChild(new ncurses\component\textline(15, 2, 20));
+		$this->addChild(new ncurses\widget\label(2, 1, 'Hostname'));
+		$this->addChild(new ncurses\widget\textline(15, 1, 20));
+		$this->addChild(new ncurses\widget\label(2, 2, 'Domain Name'));
+		$this->addChild(new ncurses\widget\textline(15, 2, 20));
 
-		$this->addChild(new ncurses\component\label(2, 4, 'Netmask'));
-		$this->addChild(new ncurses\component\textline(15, 4, 20));
-		$this->addChild(new ncurses\component\label(2, 5, 'Gateway'));
-		$this->addChild(new ncurses\component\textline(15, 5, 20));
-		$this->addChild(new ncurses\component\label(2, 6, 'Primary DNS'));
-		$this->addChild(new ncurses\component\textline(15, 6, 20));
+		$this->addChild(new ncurses\widget\label(2, 4, 'Netmask'));
+		$this->addChild(new ncurses\widget\textline(15, 4, 20));
+		$this->addChild(new ncurses\widget\label(2, 5, 'Gateway'));
+		$this->addChild(new ncurses\widget\textline(15, 5, 20));
+		$this->addChild(new ncurses\widget\label(2, 6, 'Primary DNS'));
+		$this->addChild(new ncurses\widget\textline(15, 6, 20));
 
-		$this->addChild(new ncurses\component\checkbox(2, 8, array(array('label' => 'Use HTTP Proxy',  'selected' => false))));
-		$this->addChild(new ncurses\component\label(2, 9, 'Proxy URL'));
-		$this->addChild(new ncurses\component\textline(15, 9, 20));
+		$this->addChild(new ncurses\widget\checkbox(2, 8, array(array('label' => 'Use HTTP Proxy',  'selected' => false))));
+		$this->addChild(new ncurses\widget\label(2, 9, 'Proxy URL'));
+		$this->addChild(new ncurses\widget\textline(15, 9, 20));
 
-        $this->addChild(new ncurses\component\button( 2, 12, 'Save'))->addEvent('action', function() {
+        $this->addChild(new ncurses\widget\button( 2, 12, 'Save'))->addEvent('action', function() {
             $this->doExit('Save');
         });;
-        $this->addChild(new ncurses\component\button(10, 12, 'Cancel'))->addEvent('action', function() {
+        $this->addChild(new ncurses\widget\button(10, 12, 'Cancel'))->addEvent('action', function() {
             $this->doExit('Cancel');
         });;
 	}
