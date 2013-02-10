a8a24a033799d21cce09216b80be1f5891705ae3
Harald Lapp <harald.lapp@gmail.com>
2013-02-10 12:32:27 +0100

    initial commit script for testing window scrolling

---
+++ demo/misc/scroll.php
@@ -0,0 +1,28 @@
+#!/usr/bin/env php
+<?php
+
+// initialize ncurses
+ncurses_init();
+// ncurses_noecho();
+
+register_shutdown_function(function() {
+    ncurses_end();
+});
+
+ncurses_newwin(0, 0, 0, 0);
+
+$height = 5;
+$width  = 7;
+
+$win = ncurses_newwin($height, $width, 10, 10);
+ncurses_scrollok($win, true);
+
+for ($y = 0; $y < 20; ++$y) {
+	$text = sprintf("Row #%d\n", $y + 1);
+
+	ncurses_mvwaddstr($win, ($y < $height ? $y : $height - 1), 0, substr($text, 0, $width));
+	ncurses_wrefresh($win);
+	sleep(1);
+}
+
+ncurses_delwin($win);
