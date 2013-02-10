9e06f82676636f9687526d40999e8f3c0b071103
Harald Lapp <harald.lapp@gmail.com>
2013-02-11 00:37:45 +0100

    improved readline test-script

--- demo/misc/readline.php
+++ demo/misc/readline.php
@@ -12,12 +12,32 @@ register_shutdown_function(function() {
 ncurses_newwin(0, 0, 0, 0);
 
 // readline callback handler
-function rl_callback($ret) {
+function rl_callback($line) {
+    $line = rtrim($line);
+
+    if ($line != '') readline_add_history($line);
+}
+
+function rl_info() {
+    $info = readline_info();
+    $info['history_items'] = count(readline_list_history());
+
+    $row = 2;
+    array_walk($info, function(&$v, $k) use (&$row) {
+        ncurses_mvaddstr($row++, 0, sprintf("%-20s = %s", $k, $v));
+        ncurses_clrtoeol();
+    });
+
+    ncurses_move(2, 23 + $info['point']);
+    ncurses_refresh();
 }
 
 readline_callback_handler_install('', 'rl_callback');
 
-ncurses_mvaddstr(0, 0, "press CTRL+C to exit");
+ncurses_mvaddstr(0, 0, "press any key (CTRL+C to exit)");
+ncurses_refresh();
+
+rl_info();
 
 while (true) {
     $w = NULL;
@@ -28,14 +48,6 @@ while (true) {
     if ($n && in_array(STDIN, $r)) {
         readline_callback_read_char();
 
-        $info = readline_info();
-
-        $row = 2;
-        array_walk($info, function(&$v, $k) use (&$row) {
-            ncurses_mvaddstr($row++, 0, sprintf("%-20s = %s", $k, $v));
-            ncurses_clrtoeol();
-        });
-
-        ncurses_refresh();
+        rl_info();
     }
 }
